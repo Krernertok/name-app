d39bf4d1c0e3f57a0c6aa66e49ba2d1ff8482d75
Antti Sivula <antti.j.sivula@gmail.com>
2021-01-05 21:35:23 +0200

    Refactor data.clj and add tests for untested functions

--- src/name_app/data.clj
+++ src/name_app/data.clj
@@ -1,35 +1,36 @@
 (ns name-app.data
   (:require [clojure.data.json :as json]))
 
-(def ^:private names (get 
-            (json/read-str (slurp "data/names.json")
-                           :key-fn keyword) 
-            :names))
+(def ^:private names (get
+                      (json/read-str (slurp "data/names.json")
+                                     :key-fn keyword)
+                      :names))
 
-(def ^:private names-by-amount (reverse (sort-by last names)))
+(defn- names-by-amount [] (reverse (sort-by last names)))
 
-(def ^:private names-by-name (sort-by first names))
+(defn- names-by-name [] (sort-by first names))
 
-(def ^:private total-names (reduce 
-                  (fn [total name-map] (+ total (:amount name-map)))
-                  0
-                  names))
+(defn- total-names []
+  (reduce
+   (fn [total name-map] (+ total (:amount name-map)))
+   0
+   names))
 
 
 (defn get-names-by-amount
   "Returns a list of name maps sorted according to :amount"
   []
-  names-by-amount)
+  (names-by-amount))
 
 (defn get-names-by-name
   "Returns a list of name maps sorted according to :name"
   []
-  names-by-name)
+  (names-by-name))
 
 (defn get-total-names
   "Returns the sum of the amounts of names"
   []
-  total-names)
+  (total-names))
 
 (defn get-amount-for-name
   "Returns the amount of the given name or nil if the name is not found"
--- test/name_app/data_test.clj
+++ test/name_app/data_test.clj
@@ -1,10 +1,34 @@
 (ns name-app.data-test
-  (:require [clojure.test :refer [deftest testing is]]
-            [name-app.data :refer [get-amount-for-name]]))
+  (:require [clojure.test :refer [deftest testing is use-fixtures]]
+            [name-app.data :refer :all]))
+
+(defn test-fixture [f]
+  (with-redefs [name-app.data/names '({:name "Michael" :amount 3}
+                                      {:name "Roger" :amount 4}
+                                      {:name "John" :amount 2})]
+    (f)))
+
+(use-fixtures :once test-fixture)
+
+(deftest test-get-names-by-amount
+  (testing "Getting names sorted by amount"
+    (is (= (get-names-by-amount) '({:name "Roger" :amount 4}
+                                   {:name "Michael" :amount 3}
+                                   {:name "John" :amount 2})))))
+
+(deftest test-get-names-by-name
+  (testing "Getting names sorted by name"
+    (is (= (get-names-by-name) '({:name "John" :amount 2}
+                                 {:name "Michael" :amount 3}
+                                 {:name "Roger" :amount 4})))))
+
+(deftest test-get-total-names
+  (testing "Get sum total of amounts"
+    (is (= (get-total-names) 9))))
 
 (deftest test-get-name
   (testing "Getting amount by name"
     (testing "Existing name"
-      (is (= (get-amount-for-name "Henna") 4)))
+      (is (= (get-amount-for-name "Michael") 3)))
     (testing "Non-existing name"
       (is (= (get-amount-for-name "Archibald") nil)))))
