2d8486d1c268f9290d46d4a3cd7ed7c59249f2bf
Antti Sivula <antti.j.sivula@gmail.com>
2021-01-05 19:32:28 +0200

    Add functions to process the data in names.json

---
+++ src/name_app/data.clj
@@ -0,0 +1,37 @@
+(ns name-app.data
+  (:require [clojure.data.json :as json]))
+
+(def ^:private names (get 
+            (json/read-str (slurp "data/names.json")
+                           :key-fn keyword) 
+            :names))
+
+(def ^:private names-by-amount (reverse (sort-by last names)))
+
+(def ^:private names-by-name (sort-by first names))
+
+(def ^:private total-names (reduce 
+                  (fn [total name-map] (+ total (:amount name-map)))
+                  0
+                  names))
+
+
+(defn get-names-by-amount
+  "Returns a list of name maps sorted according to :amount"
+  []
+  names-by-amount)
+
+(defn get-names-by-name
+  "Returns a list of name maps sorted according to :name"
+  []
+  names-by-name)
+
+(defn get-total-names
+  "Returns the sum of the amounts of names"
+  []
+  total-names)
+
+(defn get-name
+  "Returns the amount of the given name or nil if the name is not found"
+  [name] 
+  (:amount (first (filter #(= (:name %) name) names))))
