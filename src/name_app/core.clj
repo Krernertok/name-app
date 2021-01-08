d10d1bafbf22346f347fd0fb9b3d860d58dadc46
Antti Sivula <antti.j.sivula@gmail.com>
2021-01-09 00:13:10 +0200

    Add default handler (HTTP 404/405)

--- src/name_app/core.clj
+++ src/name_app/core.clj
@@ -22,7 +22,7 @@
         {:parameters {:query {(ds/opt :sort-by) string?}}
          :responses {200 {:body string?}}
          :handler handlers/get-names-handler}}]
-      
+
       ["/names/:name"
        {:get
         {:parameters {:path {:name string?}}
@@ -32,15 +32,18 @@
       ["/total-names"
        {:get {:handler handlers/get-total-names-handler}}]]]
 
-      {:data {:coercion reitit.coercion.spec/coercion
-              :muuntaja m/instance
-              :middleware [parameters/parameters-middleware
-                           muuntaja/format-negotiate-middleware
-                           muuntaja/format-response-middleware
-                           exception/exception-middleware
-                           muuntaja/format-request-middleware
-                           coercion/coerce-response-middleware
-                           coercion/coerce-request-middleware]}})))
+    {:data {:coercion reitit.coercion.spec/coercion
+            :muuntaja m/instance
+            :middleware [parameters/parameters-middleware
+                         muuntaja/format-negotiate-middleware
+                         muuntaja/format-response-middleware
+                         exception/exception-middleware
+                         muuntaja/format-request-middleware
+                         coercion/coerce-response-middleware
+                         coercion/coerce-request-middleware]}})
+   (ring/create-default-handler
+    {:not-found (constantly {:status 404, :body "Resource not found"})
+     :method-not-allowed (constantly {:status 405, :body "Method not allowed"})})))
                            
 
 (defn start
